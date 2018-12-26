a0abb0ee5219ca44bac65f38d7f24a577ae7ef34
Benjamin Snellings <benjaminsnellings@Benjamins-MBP.localdomain>
2018-12-26 02:10:01 -0500

    update aiohttp for new versions of python

--- pydest/pydest.py
+++ pydest/pydest.py
@@ -17,7 +17,6 @@ class Pydest:
         self.api = API(api_key, self._session)
         self._manifest = Manifest(self.api)
 
-
     async def decode_hash(self, hash_id, definition, language="en"):
         """Get the corresponding static info for an item given it's hash value from the Manifest
 
@@ -37,7 +36,6 @@ class Pydest:
         """
         return await self._manifest.decode_hash(hash_id, definition, language)
 
-
     async def update_manifest(self, language='en'):
         """Update the manifest if there is a newer version available
 
@@ -47,9 +45,8 @@ class Pydest:
         """
         await self._manifest.update_manifest(language)
 
-
-    def close(self):
-        self._session.close()
+    async def close(self):
+        await self._session.close()
 
 
 class PydestException(Exception):
